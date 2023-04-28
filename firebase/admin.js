2c0ad9a7d5e35c5db5c15582d8c9b551cd9c46d6
Doble-2 <angelcalderon303@gmail.com>
2023-04-27 20:30:44 -0400

    secure

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,7 +1,7 @@
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./serviceAccountKey.json");
+var serviceAccount =  require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
 
 try{
     admin.initializeApp({
