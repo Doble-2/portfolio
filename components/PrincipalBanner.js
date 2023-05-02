c0d1e54df6c7443e5211527c513d2724d8cf6e32
Doble-2 <angelcalderon303@gmail.com>
2023-05-02 19:44:28 -0400

    ss

--- components/PrincipalBanner.js
+++ components/PrincipalBanner.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function PrincipalBanner() {
   return (
     <section class=" px-10">
-    <div class="grid  sm:flex max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12" >
+    <div class="grid  sm:flex max-w-screen-xl px-8 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16  lg:px-16 lg:grid-cols-12" >
         <div class="lg:mr-20 place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-4xl ">Hola me llamo Angel Calderon</h1>
             <p class="max-w-2xl mb-6 font-light lg:mb-8 text-lg  md:text-lg lg:text-xl ">soy un desarrollador de software apasionado por los retos y el aprendizaje constante. Me encanta crear sitios web atractivos, funcionales y accesibles.</p>
