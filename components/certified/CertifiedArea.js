14d3e9b8570d97244b6bfedb9309daee1de6b005
Doble-2 <angelcalderon303@gmail.com>
2023-05-11 10:54:51 -0400

    65

--- components/certified/CertifiedArea.js
+++ components/certified/CertifiedArea.js
@@ -6,7 +6,7 @@ export default function ProjectArea({data}) {
     
     <div className='p-6 cardsArea' id='certifiedArea'>
         <h3 className='text-center text-2xl font-bold mb-4'>Algunos certificados que he conseguido</h3>
-        <CertifiedCarousel  data-aos="fade-down" data={data}/>
+        <CertifiedCarousel  data-aos="fade-up" data={data}/>
         <hr className='bg-gray-800'></hr>
     </div>
    
