28a32f5c6015ed7aa7b302f2303af68f26ca996f
Doble-2 <angelcalderon303@gmail.com>
2023-05-18 20:30:46 -0400

    v1.1

--- components/PrincipalBanner.js
+++ components/PrincipalBanner.js
@@ -6,7 +6,7 @@ export default function PrincipalBanner() {
     <section className=" px-10">
     <div className="grid  sm:flex max-w-screen-xl px-8 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16  lg:px-16 lg:grid-cols-12" >
         <div className="lg:mr-20 place-self-center lg:col-span-7">
-            <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-4xl ">Hola soy Ángel Calderón</h1>
+            <h1 className="max-w-2xl mb-4 tracking-tight leading-none text-2xl font-bold  md:text-1xl xl:text-2xl ">Hola soy<p className=' text-3xl font-extrabold  md:text-3xl xl:text-4xl '>Ángel Calderón</p> </h1>
             <p className="max-w-2xl mb-6 font-light lg:mb-8 text-lg  md:text-lg lg:text-xl ">un desarrollador de software apasionado por los retos y el aprendizaje constante. Tengo 21 años y actualmente resido en Caracas Venezuela</p>
             <div className='hidden sm:block'>
             <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 ">
